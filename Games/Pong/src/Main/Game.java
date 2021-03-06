bfffd093c123dd74f68d321f6245e4552c8b360a
radigurev <60190995+radigurev@users.noreply.github.com>
2021-03-06 16:24:44 +0200

    Add files via upload

---
+++ Games/Pong/src/Main/Game.java
@@ -0,0 +1,11 @@
+package Main;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class Game {
+
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+}
